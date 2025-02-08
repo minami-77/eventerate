94c1197b8e20c2ae1cfa57cd08137055a01552b4
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-02-08 16:56:42 +0900

    updated the event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,14 @@
 class EventsController < ApplicationController
   def new
     @event = Event.new
+    authorize @event
   end
 
   def create
     @event = Event.new(event_params)
     @event.user = current_user
     @event.organization = current_user.organization
+    authorize @event
     if @event.save
       redirect_to root_path
     else
