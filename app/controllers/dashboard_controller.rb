2e1e0d3a4b3259dbc9b7cb65fb4b94649af33c3d
Allan Sechrist <sechristallan@gmail.com>
2025-02-10 07:52:02 +0900

    Add button to create a new event (no link). Remove skip_before_actionauthenticate_user from dashboard controller.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,4 @@
 class DashboardController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
   def index
     @events = policy_scope(current_user.collaborated_events)
     @tasks = policy_scope(current_user.tasks)
