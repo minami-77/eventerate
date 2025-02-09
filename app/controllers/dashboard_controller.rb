26090b4dd051a2aa961764c0fff7688b0a920e26
Allan Sechrist <sechristallan@gmail.com>
2025-02-09 19:31:12 +0900

    add policy_scope for dashboard, In user.rb changed events to owned_events and added collaborated_events as queries.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
+    @events = policy_scope(current_user.collaborated_events)
+    @tasks = policy_scope(current_user.tasks)
   end
 end
