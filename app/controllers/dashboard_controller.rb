09f5e5f28d90c37d66a294b9e396bb91a6cb8617
Allan Sechrist <sechristallan@gmail.com>
2025-02-11 10:46:05 +0900

    Update dashboard controller to get user tasks for the next event in their event list. Add styling for tasks on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,12 @@
 class DashboardController < ApplicationController
   def index
-    @events = policy_scope(current_user.collaborated_events)
-    @tasks = policy_scope(current_user.tasks)
+    @events = policy_scope(current_user.collaborated_events).order(date: :asc)
+
+    if @events.any?
+      first_event = @events.first
+      @tasks = policy_scope(first_event.tasks.joins(:tasks_users).where(tasks_users: { user_id: current_user.id }))
+    else
+      @tasks = []
+    end
   end
 end
