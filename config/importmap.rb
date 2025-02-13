a6a66715f3b738028a17fffb9144d468553e28a4
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-02-13 21:29:27 +0900

    updated

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,11 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+
+pin "fullcalendar", to: "https://cdn.jsdelivr.net/npm/@fullcalendar/core@5.10.1/main.min.js"
+pin "fullcalendar-daygrid", to: "https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@5.10.1/main.min.js"
+pin "fullcalendar-timegrid", to: "https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid@5.10.1/main.min.js"
+pin "fullcalendar-interaction", to: "https://cdn.jsdelivr.net/npm/@fullcalendar/interaction@5.10.1/main.min.js"
+pin "fullcalendar-css", to: "https://cdn.jsdelivr.net/npm/@fullcalendar/core@5.10.1/main.min.css"
+pin "fullcalendar-daygrid-css", to: "https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@5.10.1/main.min.css"
+pin "fullcalendar-timegrid-css", to: "https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid@5.10.1/main.min.css"
