3eee858acc69fdc022506580f7189747284d48ab
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-02-15 13:06:06 +0900

    changed the genre to genres

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@ class Activity < ApplicationRecord
   has_many :activities_events, dependent: :destroy
   has_many :events, through: :activities_events
 
-  validates :title, :age, :genre, presence: true
+  validates :title, :age, :genres, presence: true
 
   include PgSearch::Model
 
