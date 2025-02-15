975a3617a1a9810b81d274d6ae8f8c3c3cfd375d
Minami Takayama <m.inam.ina03037@gmail.com>
2025-02-15 13:28:08 +0900

    middle of something

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,22 @@
 class TasksController < ApplicationController
   before_action :skip_authorization, only: [:update]
 
+  def create
+    @event = Event.find(params[:event_id])
+    @task = @event.tasks.new
+    @users = User.all
+    if @task.save(tasks_params)
+      # update assigned user
+      assign_user = params[:task][:user_id]
+      task_user = TasksUser.find_by(task_id: @task.id)
+      task_user.update(user_id: assign_user)
+
+      redirect_to event_path(@event), notice: "Task created successfully."
+    else
+      flash[:alert] = @task.errors.full_messages.to_sentence
+    end
+  end
+
   def update
     @event = Event.find(params[:event_id])
     @task = @event.tasks.find(params[:id])
--- app/javascript/controllers/assign_tasks_controller.js
+++ app/javascript/controllers/assign_tasks_controller.js
@@ -5,8 +5,5 @@ export default class extends Controller {
   static targets = ["form"]
   connect() {
   }
-  assign(){
-    this.formTarget.classList.toggle("d-none");
-  }
 
 }
