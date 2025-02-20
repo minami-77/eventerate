79a5d195490b800ee92b5c6445effedd54937b57
Allan Sechrist <sechristallan@gmail.com>
2025-02-20 18:53:02 +0900

    Change custom_description column from a string to a text.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_19_112445) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_20_095126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_19_112445) do
     t.datetime "updated_at", null: false
     t.json "assigned_users", default: []
     t.string "custom_title"
-    t.string "custom_description"
+    t.text "custom_description"
     t.index ["activity_id"], name: "index_activities_events_on_activity_id"
     t.index ["event_id"], name: "index_activities_events_on_event_id"
   end
