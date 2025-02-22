d05f7c8f6e223f865606370d8f133372bfde9668
Minami Takayama <m.inam.ina03037@gmail.com>
2025-02-22 18:42:04 +0900

    column problem

--- app/javascript/controllers/assign_tasks_controller.js
+++ app/javascript/controllers/assign_tasks_controller.js
@@ -2,10 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="assign-tasks"
 export default class extends Controller {
-  static targets = ["form", "titleInput"]
+  static targets = ["form", "details","column", "titleInput"]
   connect() {
     console.log("AssignTasksController connected!");
   }
+//Details
+  showDetails(){
+    this.strech();
+    this.detailsTarget.classList.toggle("d-none");
+
+  }
+  strech(){
+    console.log(this.columnTarget);
+    this.columnTarget.classList.replace("col-3","col-6");
+  }
+
 // Edit
   assign(){
     this.formTarget.classList.toggle("d-none");
