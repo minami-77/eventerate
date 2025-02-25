9a59a1b8e721bab2bc077625e24a568120cb123d
Alexander Wong <alexwongsydney@gmail.com>
2025-02-25 22:09:43 +0900

    Added functionality to filter dates when clicking on a calendar date

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,16 +1,8 @@
 class DashboardController < ApplicationController
   def index
-    @start_date = params[:start_date]&.to_date || Date.today
-    # Fetch events within the selected month, including owned and collaborated events
-    # @events = Event.where(date: @start_date.beginning_of_month..@start_date.end_of_month)
-    #           .where(user: current_user)
-    #           .or(Event.where(id: current_user.collaborated_events.ids))
-    #           .order(date: :asc).limit(3)
-
-    # @events = current_user.collaborated_events.where("date > ?", Date.current).order(date: :asc).limit(3)
     @event = Event.new
-    # @events = policy_scope(Event.where(user: current_user).or(Event.where(id: current_user.collaborated_events.ids)).order(date: :asc).limit(3))
-    @events = policy_scope(current_user.collaborated_events.where("date > ?", Date.current).order(date: :asc).limit(3))
+    @date = params["date"]
+    @events = policy_scope(current_user.collaborated_events.where("date >= ?", (@date || Date.current)).order(date: :asc).limit(3))
 
     if @events.any?
       first_event = @events.first
