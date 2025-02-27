5b72e302b118cd845879efa0ae834697d963ed9e
Allan Sechrist <sechristallan@gmail.com>
2025-02-27 21:42:29 +0900

    change index method to also return the next 4 upcoming events.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,9 @@
 class DashboardController < ApplicationController
   def index
     @event = Event.new
-    @date = params["date"]
-    @events = policy_scope(current_user.collaborated_events.where("date >= ?", (@date || Date.current)).order(date: :asc).limit(3))
+    @date = params["start_date"]
+    @events = policy_scope(current_user.collaborated_events.where("date >= ?", (@date || Date.current)).order(date: :asc))
+    @upcoming_events = policy_scope(current_user.collaborated_events.where("date >= ?", Date.current).order(date: :asc).limit(4))
     # @events = policy_scope(Event.where(user: current_user).or(Event.where(id: current_user.collaborated_events.ids)).order(date: :asc).limit(4))
 
     if @events.any?
