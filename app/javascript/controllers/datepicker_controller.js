985c6642f8180a771d922cbe3eb38710b6fe5649
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-02-28 14:25:58 +0900

    able to make the calendar showing

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,7 +3,22 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static targets = ["input"];
+
   connect() {
-    flatpickr(this.element)
+    this.initializeDatepicker();
+  }
+
+  initializeDatepicker() {
+    flatpickr(this.inputTarget, {
+      inline: true,
+      dateFormat: "Y-m-d",
+      onChange: this.onDateChange.bind(this)
+    });
+  }
+
+  onDateChange(selectedDates, dateStr, instance) {
+    // Trigger the change event to update the form
+    this.inputTarget.dispatchEvent(new Event('change'));
   }
 }
