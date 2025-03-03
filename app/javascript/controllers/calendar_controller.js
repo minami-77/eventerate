43de28680a97a7c2e67f630d5f9163fe95e46faf
Allan Sechrist <sechristallan@gmail.com>
2025-03-03 10:59:59 +0900

    When clicking on a day with no event on the calendar, the date of the day will be filled in on the event creation form.

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -2,22 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="calendar"
 export default class extends Controller {
-  // static values = {
-  //   hasEvent: Boolean,
-  //   eventId: Number
-  // }
-
-  // openModal() {
-  //   if (this.hasEventValue) {
-  //     const modalElement = document.getElementById("dateEventModal")
-  //     if (modalElement) {
-  //       const eventModal = new bootstrap.Modal(modalElement)
-  //       eventModal.show()
-  //     }
-  //   }
-  // }
-  // filterDate(event) {
-  //   const date = event.target.closest(".calendar-day").dataset.date;
-  //   window.location.href = `http://localhost:3000/dashboard?date=${date}`
-  // }
+  sendDate(event) {
+    const newDate = event.currentTarget.dataset.date;
+    const updateEvent = new CustomEvent("datepicker:update", {
+      detail: { date: newDate },
+      bubbles: true
+    });
+    document.dispatchEvent(updateEvent);
+  }
 }
--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -7,10 +7,15 @@ export default class extends Controller {
 
   connect() {
     this.initializeDatepicker();
+    document.addEventListener("datepicker:update", this.handleDateUpdate.bind(this));
+  }
+
+  disconnect() {
+    document.removeEventListener("datepicker:update", this.handleDateUpdate.bind(this));
   }
 
   initializeDatepicker() {
-    flatpickr(this.inputTarget, {
+    this.fp = flatpickr(this.inputTarget, {
       inline: true,
       dateFormat: "Y-m-d",
       minDate: "today",
@@ -21,5 +26,13 @@ export default class extends Controller {
   onDateChange(selectedDates, dateStr, instance) {
     // Trigger the change event to update the form
     this.inputTarget.dispatchEvent(new Event('change'));
+  }
+
+  handleDateUpdate(event) {
+    const newDate = event.detail.date;
+    console.log(newDate);
+    if (this.fp && newDate) {
+      this.fp.setDate(newDate, true);
+    }
   }
 }
