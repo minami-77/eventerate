f96e34c2608e6c448eb956c8db8d2a70ed1ba2ff
Allan Sechrist <sechristallan@gmail.com>
2025-03-03 09:30:28 +0900

    Add task modal, add ability to click on a day to open either the task modal if events exsist, or create event modal if there are no events

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -2,9 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="calendar"
 export default class extends Controller {
-  connect() {
-  }
+  // static values = {
+  //   hasEvent: Boolean,
+  //   eventId: Number
+  // }
 
+  // openModal() {
+  //   if (this.hasEventValue) {
+  //     const modalElement = document.getElementById("dateEventModal")
+  //     if (modalElement) {
+  //       const eventModal = new bootstrap.Modal(modalElement)
+  //       eventModal.show()
+  //     }
+  //   }
+  // }
   // filterDate(event) {
   //   const date = event.target.closest(".calendar-day").dataset.date;
   //   window.location.href = `http://localhost:3000/dashboard?date=${date}`
