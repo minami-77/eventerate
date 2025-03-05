17a84f2e7504c271e4e8b29611a238580cc0a614
Alexander Wong <alexwongsydney@gmail.com>
2025-03-05 19:10:42 +0900

    Fixed issue with removing dropdown logic where when clicking inside the dropdown, the dropdown was also removed

--- app/javascript/controllers/remove_update_dropdown_controller.js
+++ app/javascript/controllers/remove_update_dropdown_controller.js
@@ -8,7 +8,7 @@ export default class extends Controller {
 
   closeDropdown(target) {
     // Checks if there is a dropdown active. Only non assigned user images trigger this function
-    if (!target.classList.contains("assigned-user-image")) {
+    if (!target.classList.contains("assigned-user-image") && !target.closest(".tasks-users-dropdown")) {
       const currentActive = document.querySelector(".active-dropdown");
       if (currentActive) {
         currentActive.classList.remove("active-dropdown");
