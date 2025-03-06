55cba286ce2e148632c9ffba03bef8bb94f7e260
Alexander Wong <alexwongsydney@gmail.com>
2025-03-06 15:33:48 +0900

    Added a button to create a new activity and associated tasks via the activities controller, but unable to test yet due to previous branch not being merged

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,31 @@
+class ActivitiesController < ApplicationController
+
+  def new_activity_with_ai
+    @event = Event.find(params["id"])
+    event_title = params["event_title"]
+    age = params["age"]
+    generated_activity = RegenerateActivityService.regenerate_activity(event_title, age)
+
+    new_activity = @event.activities.new(
+      title: params["title"],
+      description: params["description"],
+      instructions: params["instructions"].to_json,
+      materials: params["materials"].to_json
+    )
+
+    if new_activity.save
+      params["tasks"].each do |task|
+        create_task(task, new_activity)
+      end
+    end
+
+    redirect_to event_path(@event), notice: 'Activity and tasks created successfully.'
+  end
+
+  private
+
+  def create_task(task, activity)
+    new_task = activity.tasks.new(title: task)
+    new_task.save
+  end
+end
